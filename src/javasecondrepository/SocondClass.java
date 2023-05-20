1c70d4f42cb5b0769b34df3bbbce408386a381e4
shiromarzai <shahryar_omarzai@hotnmail.com>
2023-05-20 12:36:53 -0700

    last homwork

---
+++ src/javasecondrepository/SocondClass.java
@@ -0,0 +1,10 @@
+package javasecondrepository;
+
+public class SocondClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("second Homework added");
+	}
+
+}
